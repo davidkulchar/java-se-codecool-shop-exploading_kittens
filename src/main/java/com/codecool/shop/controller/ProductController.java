67ea531e196b227ca2792e5a285e82ad4064f1a8
Péter Vetési <vetesipeti@gmail.com>
2017-04-26 15:53:24 +0200

    ProductController.java: implementation for filtering by category

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -10,23 +10,44 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
+import com.google.common.collect.Lists;
 import spark.Request;
 import spark.Response;
 import spark.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ProductController {
 
-    public static ModelAndView renderProducts(Request req, Response res) {
+    public static ModelAndView renderHomePage(Request req, Response res) {
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+
+        Map params = new HashMap<>();
+        params.put("category", productCategoryDataStore.getAll());
+        params.put("products", newProducts());
+        return new ModelAndView(params, "product/index");
+    }
+
+    private static List<Product> newProducts(){
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        List<Product> newProducts = new ArrayList<>();
+        List<Product> reverseProducts = Lists.reverse(productDataStore.getAll());
+
+        for (int i = 0; i < 3; i++){
+            newProducts.add(reverseProducts.get(i));
+        }
+        return newProducts;
+    }
+
+    public static ModelAndView renderProductsByCategory(Request req, Response res) {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
 
         Map params = new HashMap<>();
-//        params.put("category", productCategoryDataStore.find(1));
-        params.put("category", productCategoryDataStore.getAll());
-        params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+        ProductCategory category = productCategoryDataStore.find(req.params(":name"));
+        params.put("category", category);
+        params.put("products", productDataStore.getBy(category));
         return new ModelAndView(params, "product/index");
     }
 }
+
