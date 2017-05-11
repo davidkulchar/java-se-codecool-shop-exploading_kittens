928330774483a5b0bc71391dcd672e36cc011882
Péter Vetési <vetesipeti@gmail.com>
2017-05-11 12:55:27 +0200

    product creation relocated to the end of sql file

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -1,29 +1,29 @@
-DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS product, supplier, product_category;
 
-CREATE TABLE Product
-(
-  id SERIAL PRIMARY KEY,
-  defaultPrice FLOAT,
-  defaultCurrency VARCHAR(255),
-  pic VARCHAR(255),
-  name VARCHAR(255),
-  description VARCHAR(255),
-  supplier INTEGER REFERENCES Supplier,
-  productCategory INTEGER REFERENCES ProductCategory
-);
-
-CREATE TABLE Supplier
+CREATE TABLE supplier
 (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   description VARCHAR(255)
 );
 
-CREATE TABLE ProductCategory
+CREATE TABLE product_category
 (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   description VARCHAR(255),
   department VARCHAR(255)
+);
+
+CREATE TABLE product
+(
+  id SERIAL PRIMARY KEY,
+  default_price FLOAT,
+  default_currency VARCHAR(255),
+  pic VARCHAR(255),
+  name VARCHAR(255),
+  description VARCHAR(255),
+  supplier INTEGER REFERENCES supplier,
+  product_category INTEGER REFERENCES product_category
 );
 
